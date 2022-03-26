f3ddd9ed4c917160939b70340698b9ffd06e22e3
Davis06 <davis06@outlook.com>
2022-03-26 01:41:48 +0100

    completed accordion app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Question from "./components/Question";
+import Questions from "./components/Question";
+import { data } from "./data";
 
 function App() {
+
+  const [infoData, setInfoData] = useState(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app bg-slate-50 py-6 my-16 mx-4 rounded-sm h-auto lg:w-[80vw] lg:mx-auto">
+      <div className='main-container flex flex-col mx-6 lg:flex-row lg:justify-between'>
+        <div className="Accordion-info">
+          <h1 className="info mb-3 font-medium text-[1.25rem] text-center lg:text-left lg:text-[1.75rem]">
+            Questions And Answers About Login
+          </h1>
+        </div>
+       <div className="questions-div flex flex-col gap-4 lg:basis-[70%]">
+          {
+            infoData.map((item) => {
+              return <Question
+                        key={item.id}
+                        item={item}
+                      />
+            })
+          }
+       </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Question.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+
+const Question = ({item}) => {
+
+  const [showInfo, setShowInfo] = useState(false)
+
+
+  const handleShow = () => {
+    setShowInfo((prevState) => !prevState)
+  }
+  return (
+    <section className='border border-gray-400 py-4 rounded-sm shadow-gray-400 shadow-sm'>
+        <div className='wrapper flex justify-between items-center mx-6'>
+            <div className='question text-[0.9rem] font-medium'>
+              {item.title}
+            </div>
+            <button 
+              onClick={handleShow}
+              className='button bg-gray-200 px-1 py-1 rounded-full text-lg'>
+              {showInfo ? <FiMinus/> :
+              <FiPlus/>}
+            </button>
+        </div>
+        <div className='answer mx-6 text-[0.75rem]'>
+              {showInfo && item.info}
+        </div>
+    </section>
+  )
+}
+
+export default Question
